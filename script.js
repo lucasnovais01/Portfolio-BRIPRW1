4f70f9872987b9685d8aeb98487635daca826633
lucasnovais01 <novais.oliveira@aluno.ifsp.edu.br>
2025-03-25 18:22:17 -0300

    Consertado

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 console.log("Teste, clicar no f12");
+
+/* O código abaixo é para os botões */
+
+document.getElementById("variaveisBtn").addEventListener("click", function() {
+    toggleSections("variavelSection");
+});
+
+document.getElementById("tipo_dadosBtn").addEventListener("click", function() {
+    toggleSections("tipoDadosSection");
+});
+
+document.getElementById("num_operadoresBtn").addEventListener("click", function() {
+    toggleSections("numOperadoresSection");
+});
+
+document.getElementById("booleansBtn").addEventListener("click", function() {
+    toggleSections("booleansSection");
+});
+
+document.getElementById("funcoesBtn").addEventListener("click", function() {
+    toggleSections("funcoesSection");
+});
+
+document.getElementById("objetosBtn").addEventListener("click", function() {
+    toggleSections("objetosSection");
+});
+
+// Função para alternar a visibilidade das seções
+function toggleSections(sectionId) {
+    // Esconde todas as seções
+    let sections = document.querySelectorAll("section[id$='Section']");
+    sections.forEach(function(section) {
+        section.style.display = "none";
+    });
+
+    // Exibe a seção selecionada
+    document.getElementById(sectionId).style.display = "block";
+}
