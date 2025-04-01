a2485977c16698bbc4026e413374e21ce0b6cde1
lucasnovais01 <novais.oliveira@aluno.ifsp.edu.br>
2025-04-01 16:18:38 -0300

    nmnovo2

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
 console.log("Abaixo estão os códigos de Javascript do index.html");
+/*
+Nesta página script.js pretendo colocar os códigos javascript diretamente na página index.html
 
-/* O código abaixo é para os botões */
+Para outras páginas vou criar outras páginas de script.js
+*/
+
+/* Entenda o que está acontencendo:
+
+document = refere-se ao documento HTML, é uma API do DOM (observação: Não funciona no WIX, eu testei, WIX não usa API DOM)
+    Através do objeto document, podemos interagir com o conteúdo da página HTML, como acessar elementos, modificar atributos, ou até mesmo alterar o conteúdo da página.
+
+getElementById(" id ") = O getElementById é um método do objeto document, usado para localizar e acessar um elemento específico da página HTML pelo seu id.
+    recebe como parâmetro o ID de um elemento HTML e retorna esse elemento, caso ele exista.
+
+addEventListener("click", function() { ... }) = é um método que permite adicionar um ouvinte de evento (um "listener") a um elemento HTML. 
+                                                Ele diz ao navegador para escutar quando um evento específico ocorrer nesse elemento.
+"click" =  é o tipo de evento que estamos dizendo ao navegador para "escutar" os cliques sobre o botão referenciado.
+function() { ... } = Será chamada quando o evento de clique ocorrer. Essa função contém o código que será executado no momento em que o evento acontecer. Isto é chamado Callback
+
+function() {
+    toggleSections(" id "); = Quando acontecer o callback, o código executa o comando toggleSections(" id ");
+
+
+toggleSections("variavelSection") chama uma outra função chamada toggleSections. Essa função está sendo usada para alternar a visibilidade de seções na página (presumivelmente, ela exibe uma seção e oculta as outras).
+
+*/
+
+// O código abaixo é para os botões do main do index.html
 
 document.getElementById("variaveisBtn").addEventListener("click", function() {
     toggleSections("variavelSection");
 });
 
-document.getElementById("tipo_dadosBtn").addEventListener("click", function() {
+document.getElementById("tipoDadosBtn").addEventListener("click", function() {
     toggleSections("tipoDadosSection");
 });
 
-document.getElementById("num_operadoresBtn").addEventListener("click", function() {
+document.getElementById("numOperadoresBtn").addEventListener("click", function() {
     toggleSections("numOperadoresSection");
 });
 
